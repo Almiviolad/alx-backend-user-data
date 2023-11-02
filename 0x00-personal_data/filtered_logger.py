60da4d825c43feebb5498a34f0aeb079e01494ce
root <mykelaloh@gmail.com>
2023-11-02 05:43:43 -0700

    changed sperator to separator

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated:
 
     Arguments:
@@ -17,6 +17,6 @@ will be obfuscated
     separator: a string representing by which character is separating
 all fields in the log line (message)"""
     for field in fields:
-        message = re.sub(f'{field}.*?{seperator}',
-                         f'{field}={redaction}{seperator}', message)
+        message = re.sub(f'{field}.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
