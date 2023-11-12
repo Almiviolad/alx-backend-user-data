8657a3d35898c8bf3785d5c95158b0fd9e9cc72e
root <mykelaloh@gmail.com>
2023-11-11 18:49:05 -0800

    task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 from flask import request
 from typing import List, TypeVar
 import re
+import os
 
 
 class Auth():
@@ -36,3 +37,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ currnt user"""
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """returns a cookie value from a request:"""
+        if request is None:
+            return None
+        cookie = request.cookies.get('_my_session_id')
+        return cookie
