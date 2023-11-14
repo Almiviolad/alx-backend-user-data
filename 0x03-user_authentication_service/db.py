ec608213296456ab6e49b68bd12d91cc241d9785
root <mykelaloh@gmail.com>
2023-11-14 12:19:18 -0800

    added return None, and movd check for attribite up

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -56,8 +56,8 @@ then will update the user’s attributes as
  passed in the method’s arguments"""
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
             setattr(user, key, value)
-        self._session.add(user)
         self._session.commit()
-        if not hasattr(user, key):
-            raise ValueError
+        return None
