4301feeccf3afa059fe0860a41cbdc6a03d8a0a7
root <mykelaloh@gmail.com>
2023-11-14 02:13:02 -0800

    changed the type to User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> TypeVar('User'):
+    def add_user(self, email, hashed_password) -> User:
         """save the user to the database and returns user object"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
