868724aa5db0dd7b2f4c5669304cc4b267888166
root <mykelaloh@gmail.com>
2023-11-14 19:52:46 -0800

    task8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,13 @@ arguments and return a User object."""
             return user
         else:
             raise ValueError('User {} already exists'.format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validaes email and password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
