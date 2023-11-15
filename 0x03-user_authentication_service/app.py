09e7d2f09354dca8a059b512c18c812e3759ee60
root <mykelaloh@gmail.com>
2023-11-14 19:22:45 -0800

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
@@ -11,6 +12,20 @@ def home() -> jsonify:
     """use flask.jsonify to return a JSON payload"""
     payload = {"message": "Bienvenue"}
     return jsonify(payload)
+
+
+@app.route("/users", methods=['POST'])
+def register_user() -> jsonify:
+    """register a user"""
+    auth = Auth()
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = auth.register_user(user.email, password)
+        data = {"email": email, "message": "user created"}
+        return jsonify(data)
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
